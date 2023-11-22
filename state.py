1b220dd3653d0028b1a72c93de401dfcb82e9813
Denzypaulito <124909699+Denzypaulito@users.noreply.github.com>
2023-11-22 01:47:42 -0600

    Adición de contador de Pelotas Perdidas

--- state.py
+++ state.py
@@ -5,6 +5,11 @@ from colors import get_rgb
 from position import get_position
 from paddle import PaddleThread
 
+pygame.init()
+
+lost_balls_count = 0
+font = pygame.font.SysFont(None, 36)
+
 screen_width = 0
 screen_height = 0
 ball_radius = 20
@@ -12,6 +17,10 @@ ball_speed = 10
 
 ball_x = 0
 ball_y = 0
+
+lost_balls_count = 0
+font = pygame.font.SysFont(None, 36)
+
 
 ball_speed_x = random.choice([-1, 1])
 ball_speed_y = 1
@@ -21,9 +30,9 @@ paddle_height = 20
 paddle_x = 0
 
 def main(pThread):
-    pygame.init()
+    #pygame.init()
 
-    global screen_width, screen_height, ball_x, ball_y, ball_speed_x, ball_speed_y, paddle_width, paddle_x
+    global lost_balls_count, screen_width, screen_height, ball_x, ball_y, ball_speed_x, ball_speed_y, paddle_width, paddle_x
     screen_width, screen_height = get_width(), get_height()
     screen = pygame.display.set_mode((screen_width, screen_height))
     ball_x = random.randint(ball_radius, screen_width - ball_radius)
@@ -60,12 +69,16 @@ def main(pThread):
             ball_speed_y *= -1
 
         if ball_y >= screen_height - ball_radius:
+            lost_balls_count += 1
             ball_x = random.randint(ball_radius, screen_width - ball_radius)
             ball_y = 0
             ball_speed_x = random.choice([-1, 1])
             ball_speed_y = 1
 
         screen.fill(get_rgb('black'))
+
+        text = font.render(f"Lost Balls: {lost_balls_count}", True, get_rgb('white'))
+        screen.blit(text, (10, 10))
 
         ball = pygame.draw.circle(screen, get_rgb('magenta'), (int(ball_x), int(ball_y)), ball_radius)
         print(get_position(ball))
